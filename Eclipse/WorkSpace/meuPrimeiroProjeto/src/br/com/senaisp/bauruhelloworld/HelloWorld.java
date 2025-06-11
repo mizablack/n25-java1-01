50e4eb6d21ce2742f610c5218f5fb97ca7e32b55
Mizael Silva <mizasoulblack@gmail.com>
2025-06-10 21:57:08 -0300

    eclipse

---
+++ Eclipse/WorkSpace/meuPrimeiroProjeto/src/br/com/senaisp/bauruhelloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package br.com.senaisp.bauruhelloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Hello World!!");
+    
+	}
+
+}
