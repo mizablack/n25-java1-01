8dd92d8a6a4d12c689882c41ded212bfc47df9d5
Mizael Silva <mizasoulblack@gmail.com>
2025-06-12 21:47:35 -0300

    aula03

--- Eclipse/WorkSpace/conversaoGalao2litros/src/br/com/senaisp/bauru/classes/Estudante.java
+++ Eclipse/WorkSpace/conversaoGalao2litros/src/br/com/senaisp/bauru/classes/Estudante.java
@@ -12,41 +12,46 @@ public class Estudante {
 		setNome("Sem nome");
 		ra = 0;
 		nota = 0.0;
-		ehFumante = false;
-		
+		ehFumante = false;	
 	}
-
+	public Estudante(int ra, String nome) {
+		setRa(ra);
+		setNome(nome);
+		nota = 0;
+		ehFumante = false;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public int getRa() {
 		return ra;
 	}
-
 	public void setRa(int ra) {
 		this.ra = ra;
 	}
-
 	public double getNota() {
 		return nota;
 	}
-
 	public void setNota(double nota) {
 		this.nota = nota;
 	}
-
 	public boolean isEhFumante() {
 		return ehFumante;
 	}
-
 	public void setEhFumante(boolean ehFumante) {
 		this.ehFumante = ehFumante;
 	}
-	
-
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Nome: " + getNome() + "\n" + 
+			   "Ra: " + getRa() + "\n" +
+		       "Note " + getNota() + "\n"  +
+			   "Fumante " + isEhFumante();
+		       		
+	}
 }
--- Eclipse/WorkSpace/conversaoGalao2litros/src/br/com/senaisp/bauru/classes/EstudanteTester.java
+++ Eclipse/WorkSpace/conversaoGalao2litros/src/br/com/senaisp/bauru/classes/EstudanteTester.java
@@ -5,7 +5,15 @@ public class EstudanteTester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Estudante est = new Estudante();
+		est.setNome("Mizael");
+		est.setRa(1983);
 		System.out.println(est.getNome());
+		System.out.println(est ); // Usa o metodo toString()
+		Estudante est2 = new Estudante(4567,"João");
+		System.out.println(est2);
+		Estudante est3 = new Estudante(9999, "Rafael");
+		System.out.println(est3);
+		
 
 	}
 
